830c3dbb7b2f0ee94da1d8a3596525217a7d2c23
Daryl Factor <darylfactor@ymail.com>
2015-09-28 22:31:31 -0700

    remove platforms android folder

--- cordova-project/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Fire\Documents\Projects\MobileWebAppsRecipesBook\src\MobileWebAppsRecipes\jQMApps\apps\confrooms-4\cordova-project\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Fire\Documents\Projects\MobileWebAppsRecipesBook\src\MobileWebAppsRecipes\jQMApps\apps\confrooms-4\cordova-project\platforms\android\CordovaLib\AndroidManifest.xml \
--- cordova-project/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Fire\Documents\Projects\MobileWebAppsRecipesBook\src\MobileWebAppsRecipes\jQMApps\apps\confrooms-4\cordova-project\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Fire\Documents\Projects\MobileWebAppsRecipesBook\src\MobileWebAppsRecipes\jQMApps\apps\confrooms-4\cordova-project\platforms\android\AndroidManifest.xml \
