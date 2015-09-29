822a098a591de32f828ce1a4ecdff08a65ba7eb3
Daryl Factor <darylfactor@ymail.com>
2015-09-28 23:51:39 -0700

    ajax req

--- cordova-project/www/forms/register.php
+++ cordova-project/www/forms/register.php
@@ -11,8 +11,10 @@ $username = "a7881654_root";
 $password = "ece211";
 
 $con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
-
 mysql_select_db($database, $con);
+
+$response_array['dberror'] = mysql_error();  
+echo json_encode($response_array);
 
 $name = mysql_real_escape_string($_POST["name"]);
 $email = mysql_real_escape_string($_POST["email"]);
@@ -30,17 +32,13 @@ $sqlcheckcount = mysql_num_rows($sqlcheck);
 if ($sqlcheckcount > 0){
 	$response_array['status'] = 1;  
 	echo json_encode($response_array);
-
 }else{
-
 	$sql = "INSERT INTO accounts (fullname, age, gender, email, password) VALUES ('$name', '$age', '$gender', '$email', '$password')";
-
 	if(mysql_query($sql)){
 		$response_array['status'] = 'success';  
 	}else {
 		$response_array['status'] = mysql_error();  
 	}
-	
 	echo json_encode($response_array);
 }
 
--- cordova-project/www/js/signup-controller.js
+++ cordova-project/www/js/signup-controller.js
@@ -189,9 +189,12 @@ BookIt.SignUpController.prototype.onSignupCommand = function () {
     $.ajax({
         type: 'POST',
         url: BookIt.Settings.signUpUrl,
+		crossDomain: true,
+		cache: false,
         data: "email=" + emailAddress + "&name=" + name + "&password=" + password + "&age=" + age + "&gender=" + gender,
         success: function (data) {
 			console.log(data.status);
+			console.log(data.dberror);
 			if(data.status == 1){
 				me.$ctnErr.html("<p>Error: Email already exists!</p>");
 				me.$ctnErr.addClass("bi-ctn-err").slideDown();
@@ -206,10 +209,12 @@ BookIt.SignUpController.prototype.onSignupCommand = function () {
         },
         error: function (data) {
 			console.log(data.status);
+			console.log(data.dberror);
             // TODO: Use a friendlier error message below.
             me.$ctnErr.html(data.status);
             me.$ctnErr.addClass("bi-ctn-err").slideDown();
 			$('html, body').animate({ scrollTop: 0 }, 'slow');
         }
     });
+	return false;
 };
